6682bc05e4ad5fd9c1779502c15c6cd14a5b271e
Sharma <shailly574@gmail.com>
2021-07-13 16:13:20 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocialAuthService } from 'angularx-social-login';
+import {GoogleLoginProvider} from 'angularx-social-login';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,13 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'first-angular-app';
   users : any = [];
-  constructor(){
 
-  }
-  ngOnInit()
-  {
+  constructor(private socialAuthService: SocialAuthService){
 
   }
 
+  loginWithGoogle(): void {
+      this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+        .then(() => console.log("Logged in"));
+    }
   // () => {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {GoogleLoginProvider,FacebookLoginProvider} from 'angularx-social-login';
 
 @NgModule({
   declarations: [
-    AppComponent,
     TodosComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true, //keeps the user signed in
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('65557985008-blvlgosia3rclh16sstieiduv0qiqplc.apps.googleusercontent.com') // your client id
+        }
+      ]
+    }
+  },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
